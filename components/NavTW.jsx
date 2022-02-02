22e9a38e3b0c380dbe73e642308c7e84d6022fa9
Philip Beauford <PhilipBeauford@github.com>
2022-02-02 15:36:55 -0800

    blob effects

--- components/NavTW.jsx
+++ components/NavTW.jsx
@@ -463,10 +463,12 @@ export default function NavTW() {
                   <div>
                     <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                       <span className="sr-only">Open user menu</span>
-                      <img
+                      <Image
                         className="h-8 w-8 rounded-full"
                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                         alt=""
+                        height={32}
+                        width={32}
                       />
                     </Menu.Button>
                   </div>
