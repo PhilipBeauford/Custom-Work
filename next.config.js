d4b6835e4c3b53d9218610372ee9fe34bb1fb910
Philip Beauford <PhilipBeauford@github.com>
2022-02-10 21:18:33 -0800

    updates

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  env: {
+    SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+    SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN
+},
   images: {
     domains: ['cdn.shopify.com', 'images.unsplash.com', 'tailwindui.com'],
   },
