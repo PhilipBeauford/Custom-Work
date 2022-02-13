464ac45a46b05b955d1868896a147e6af6b47ebf
Philip Beauford <PhilipBeauford@github.com>
2022-02-12 22:27:18 -0800

    updates

--- components/HeroMine.jsx
+++ components/HeroMine.jsx
@@ -10,7 +10,11 @@ const HeroMine = () => {
   <div className="text-center text-white">
     <div className="max-w-md mt-56">
       <h1 className="mb-5 text-5xl font-bold whitespace-nowrap">
-            The Hottest Brands
+            The 
+            <strong style={{background: linear-gradient(115deg, #7839f3 -8%, #5433ff 18%, #20bdff 59%, #00fb9b 91%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent }}> Hottest
+            </strong>  Brands
           </h1> 
       <p className="mb-5">
             NextJS TailWind Shopify.  
